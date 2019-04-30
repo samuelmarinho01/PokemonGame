906dba37557e55104480219e207014f901a44a47
samuelmarinho01 <samuel.marinhok@gmail.com>
2019-04-30 00:42:24 -0300

    Crontroller and Battle Controls added

---
+++ PokemonTesting/src/BattleControls.java
@@ -0,0 +1,16 @@
+
+
+public class BattleControls extends Controller  {
+
+	
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		BattleControls bc = new BattleControls();	
+		
+		
+		
+		
+}
+
+}
--- PokemonTesting/src/Event.java
+++ PokemonTesting/src/Event.java
@@ -1,4 +1,5 @@
 
 public class Event {
+	private String nome;
 
 }
