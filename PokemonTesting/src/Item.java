3a53f45df87f0dbd1821c13df4b62bc83e01c4e9
samuelmarinho01 <48339772+samuelmarinho01@users.noreply.github.com>
2019-05-01 00:53:09 -0300

    Update Item.java

--- PokemonTesting/src/Item.java
+++ PokemonTesting/src/Item.java
@@ -1,5 +1,30 @@
 
 public class Item {
 	private String nome;
-	private int valor;
+	protected int valor;
+	
+	public Item (String nome, int n) {
+		this.nome = nome;
+		valor = n;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getValor() {
+		return valor;
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+}
+
+class Potion extends Item{
+	public Potion() {
+		super("Potion", 100);
+	}
 }
